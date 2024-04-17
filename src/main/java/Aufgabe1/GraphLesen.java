9533c8cfd6284af61e0361b256f5c48907e4babb
I3ahsoxx <andre.demir@haw-hamburg.de>
2024-04-17 17:26:07 +0200

    Added a Tutorial class to visualize the workflow of the Graphstream library

--- src/main/java/Aufgabe1/GraphLesen.java
+++ src/main/java/Aufgabe1/GraphLesen.java
@@ -2,4 +2,5 @@ package Aufgabe1;
 
 public class GraphLesen {
     //TODO: Implementieren Sie hier Ihre Lösung für die Methode "public static Graph readGraph(String filename)".
+    //TODO: Graphen in Matrixen speichern
 }
